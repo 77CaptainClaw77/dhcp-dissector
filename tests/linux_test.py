70a18629bf51c5528f7e854901f3def1e31d2f73
77CaptainClaw77 <kumar1235813@gmail.com>
2020-07-10 17:26:13 +0530

    Linux test added

---
+++ tests/linux_test.py
@@ -0,0 +1,22 @@
+import subprocess
+import shlex
+import threading
+import os
+import pyshark
+
+cmd="sh ../scripts/dhcp_reset.sh"
+tshark_cmd="tshark -Y 'dhcp'"
+#print(shlex.split(cmd))
+#print(shlex.split(tshark_cmd))
+proc_var=subprocess.Popen(args=['sh', '../scripts/dhcp_reset.sh'])
+capture_proc=subprocess.Popen(args=['tshark','-a','duration:30','-w', 'linux_test_output.pcap'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+capture_proc.wait()
+cap=pyshark.FileCapture(input_file='linux_test_output.pcap')
+for p in cap:
+    if 'DHCP' in p: 
+        print(p['dhcp'])
+cap.close()
+
+#proc_var=subprocess.run(['ls','-la'],stdout=subprocess.PIPE,text=True)
+# print(proc_var.stdout)
+
