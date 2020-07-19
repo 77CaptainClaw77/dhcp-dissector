6f8b03e404b8dc98475c9dabc7824fbcc7600c61
77CaptainClaw77 <kumar1235813@gmail.com>
2020-07-19 12:01:56 +0530

    added platform check and integrated live scan

--- src/dhcp_packet_processing.py
+++ src/dhcp_packet_processing.py
@@ -1,35 +1,55 @@
 import pyshark
 import sys
-import sys
+import datetime
+import subprocess
 
 ERROR_CODES={'INCORRECT_USAGE':1,'PROCESS_ERROR':2,'PLOTTING_ERROR':3,
-'UNSUPPORTED_PLATFORM':4,'UNKNOWN':5}
-DHCP_PACKET_TYPES={}
+'UNSUPPORTED_PLATFORM':4,'UNKNOWN':5,'INPUT_ERROR':6}
+DHCP_PACKET_TYPES={'01':'discover','02':'offer','03':'request','04':'decline'
+,'05':'acknowledgement','06':'negative acknowledgement','07':'release'}
+global cur_platform #hold information about current operating system
 
 def get_current_platform(): #identify current operating system
-    if sys.platform.startswith('linux')
+    if sys.platform.startswith('linux'):
         return 'linux'
-    elif sys.platform.startswith('win32')
+    elif sys.platform.startswith('win32'):
         return 'windows'
-    else:
         print('Unsupported Platform!')
+    else:
         exit(ERROR_CODES['UNSUPPORTED_PLATFORM'])
-    pass
 
-def live_port_scan_pyshark(): #scan packets in real time using pyshark
-    
-    pass
+def generate_file_name():
+    return 'live_packet_capture_'+datetime.datetime.now().strftime("%d_%m_%Y %H:%M:%S")+".pcap"
+
+# def live_port_scan_pyshark(): #scan packets in real time using pyshark
+#     live_cap_fname=generate_file_name()
+#     captured_packets= pyshark.LiveCapture(output_file=live_cap_fname)
+#     print("Starting packet capture....")
+#     captured_packets.sniff(timeout=30)
+#     if cur_platform=='linux': 
+#         proc_var=subprocess.Popen(args=['sh', '../scripts/dhcp_reset.sh'])
+#     else:
+#         proc_var=subprocess.Popen(args=['..\scripts\dhcp_reset.bat'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+#     return live_cap_fname
 
 def live_port_scan_tshark(): #scan packets in real time with tshark
+    live_cap_fname=generate_file_name()
+    print("Starting live packet capture process....")
     if cur_platform=='linux':
         cmd="sh ../scripts/dhcp_reset.sh"
         tshark_cmd="tshark -Y 'dhcp'"
+        capture_proc=subprocess.Popen(args=['tshark','-a','duration:30','-w', live_cap_fname],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         proc_var=subprocess.Popen(args=['sh', '../scripts/dhcp_reset.sh'])
-        capture_proc=subprocess.Popen(args=['tshark','-a','duration:30','-w', 'linux_test_output.pcap'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         capture_proc.wait()
     else:
-        
-    pass
+        cmd="..\scripts\dhcp_reset.bat"
+        tshark_cmd="tshark -Y 'dhcp'"
+        capture_proc=subprocess.Popen(args=['tshark','-i','Ethernet','-a','duration:30','-w', live_cap_fname],stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,shell=True)
+        proc_var=subprocess.Popen(args=['..\scripts\dhcp_reset.bat'],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        capture_proc.wait()
+    return live_cap_fname
+
 
 def scan_pcap_file(fname): #scan a file
     pass
@@ -44,9 +64,10 @@ def plot():
         print(e)
         sys.exit(ERROR_CODES['PLOTTING_ERROR'])
 
-def packet_read():
-    packet_file=sys.argv[1]
+def packet_read(packet_file):
     cap=pyshark.FileCapture(input_file=packet_file)
+    dhcp_packets=[]
+    plot_data=[]
     for pkt in cap:
         if 'DHCP' in pkt:
             print("dstn address: ",pkt['ip'].dst)
@@ -59,28 +80,24 @@ def packet_read():
                 print("client to server")
             if(pkt.dhcp.type=='2'):
                 print("server to client")
-            if(pkt.dhcp.option_value=='07'):
-                print("release")
-            elif(pkt.dhcp.option_value=='01'):
-                print("discover")
-            elif(pkt.dhcp.option_value=='02'):
-                print("offer")
-            elif(pkt.dhcp.option_value=='03'):
-                print("request")
-            elif(pkt.dhcp.option_value=='04'):
-                print("decline")
-            elif(pkt.dhcp.option_value=='05'):
-                print("acknowledgement")
-            elif(pkt.dhcp.option_value=='06'):
-                print("negative acknowledgement")
+            dhcp_packet_type=DHCP_PACKET_TYPES[pkt.dhcp.option_value]
             if(str(pkt['ip'].dst)=='255.255.255.255'):
-                print("broadcast")
+                transmission_type="broadcast"
             else:
-                print("unicast")
+                transmission_type="unicast"
+            print(dir(pkt))
+            pkt_dt_time_ob=datetime.datetime.fromtimestamp(int(float(pkt.sniff_timestamp)))
+            #plot_data.append([dhcp_packet_type,pkt.dhcp.)
     cap.close()
 
 if __name__=="__main__":
-    if len(sys.argv)!=2:
+    print("DHCP Packet Analyser") 
+    if len(sys.argv)==2:
+        print("Running in file scan mode....")
         print("Incorrect usage! Correct usage is: python <script_name>.py <pcap_file>")
         sys.exit(ERROR_CODES['INCORRECT_USAGE'])
-    packet_read()
+    else: #default option
+        print("Running in live scan mode")
+        cur_platform=get_current_platform()
+        live_cap_file=live_port_scan_tshark()  
+        packet_read(live_cap_file)
